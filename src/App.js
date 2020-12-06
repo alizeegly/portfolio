b4e0b3826b048959d8e08c3bf163351c142710f4
Alizée Gillouaye <alizee@whatizis.com>
2020-12-06 02:20:37 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
 import './App.css';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-import Navbar from './components/Navbar/Navbar';
 
 function App() {
 	return (
-		<div className="App">
-			<Router>
-				<Navbar/>
-			</Router>
-		</div>
+		<div>Hello</div>
 	);
 }
 
